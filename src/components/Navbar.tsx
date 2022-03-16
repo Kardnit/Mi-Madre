1470f73cc85c82818998b4ff3f9e39cddf4cabdb
Borga <400446@student.fontys.nl>
2022-03-16 21:44:19 +0100

    Added framer-motion for beatiful effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BarItems } from "./BarItems"
 import '../styles/Navbar.css'
+import { motion } from "framer-motion"
 
 class Navbar extends Component {
     
@@ -17,11 +18,22 @@ class Navbar extends Component {
                 <ul className='NavMenu'>
                     {BarItems.map((item,index) => {
                         return (
-                            <li key={index}>
+                            <motion.div className='LoadMotion'
+                            initial={{ opacity: 0}}
+                            animate={{ opacity: 1}}
+                            transition={{ delay: 1.5, duration: 1.5}}>
+                                
+                                <motion.li key={index}
+                                whileHover={{
+                                    scale: 1.5,
+                                }}>
                                 <a className={item.cName} href={item.url}>
                                 {item.title}
                                 </a>
-                            </li>
+                                </motion.li>
+
+                            </motion.div>
+                            
                         )
                     })}
                 </ul>
