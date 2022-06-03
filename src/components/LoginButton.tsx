a8227608eea3a0dc053c03e2d28532ccd205edaf
Kardnit <a.borgaiskender@gmail.com>
2022-06-04 00:13:06 +0200

    Added jwt to website

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
-import '../styles/Navbar.css'
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "../styles/Navbar.css";
 
 const LoginButton = () => {
-    const {loginWithRedirect, isAuthenticated} = useAuth0();
+  
+  const { loginWithRedirect, isAuthenticated} =
+    useAuth0();
 
   return (
     !isAuthenticated && (
-    <button onClick={() => loginWithRedirect()}>
-        Log In
-    </button>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
     )
-  )
-}
+  );
+};
 
-export default LoginButton
+export default LoginButton;
--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -3,10 +3,21 @@ import { useState, useEffect } from 'react'
 import {Navbar, Title, WorkTable} from '../components'
 import {motion} from "framer-motion"
 import { useAuth0 } from '@auth0/auth0-react';
+import Cookies from 'universal-cookie'
 
 export default function Panel() {
 
-  const {isLoading, isAuthenticated} = useAuth0();
+  const {isLoading, isAuthenticated, getAccessTokenSilently} = useAuth0();
+
+  useEffect(() => {
+    getAccessTokenSilently().then((response) => {
+        console.log(response)
+        const cookies = new Cookies()
+        cookies.set("jwt", response)
+    });
+  }, []);
+
+  console.log(getAccessTokenSilently());
 
   if(!isAuthenticated && !isLoading){window.location.replace('/Home')}
 
